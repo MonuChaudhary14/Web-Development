b59979c1a1184dc13f7af6f3cd9a37001b45af40
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-06-03 22:39:10 +0530

    DAY19

---
+++ JavaScript/Concepts/HTML_Collection.js
@@ -0,0 +1,9 @@
+// An HTMLCollection is an array-like object that contains a list of HTML elements. It is live, meaning it updates automatically when the DOM changes.
+
+// You often get an HTMLCollection when using certain DOM methods like:
+
+// document.getElementsByTagName()
+
+// document.getElementsByClassName()
+
+// element.children
--- JavaScript/Concepts/Window.js
+++ JavaScript/Concepts/Window.js
@@ -1,2 +1,35 @@
 // The window object in JavaScript is a global object provided by the browser when running JavaScript in a web page context. It represents the browser window or tab that contains a web page
 
+
+
+// DOM -> Document Object Model
+// The DOM (Document Object Model) is a programming interface for HTML and XML documents. It represents the page so that programs (like JavaScript) can read, manipulate, and modify the content, structure, and style of a website.
+
+/*
+
+document                Here document is the root element
+ └── html
+     ├── head
+     │   └── title
+     └── body
+         ├── h1
+         └── p
+
+*/
+
+
+// Document -> Converting the html code into js
+
+// The tags are converted into tokens by tokenizer 
+// , then these token are converted into nodes and DOM is ready  
+
+
+
+
+
+
+//BOM-> BOM stands for Browser Object Model.
+//It allows JavaScript to interact with the browser itself (not just the content of the webpage like the DOM does)
+
+// BOM is used to communicate to the browser for other matter which is not content like history , location , etc.
+
