71c5d147f931308d8cd4299aa25c0797a9985c32
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-06-05 23:51:31 +0530

    DAY21

---
+++ JavaScript/Browser_Events.js
@@ -0,0 +1,4 @@
+// In JavaScript, browser events are actions or occurrences that happen in the browser, usually triggered by the user or the browser itself, which can be responded to using event handlers.
+
+// A browser event is a signal that something has occurred, like a mouse click, key press, page load, or form submission, and JavaScript can execute code in response.
+
