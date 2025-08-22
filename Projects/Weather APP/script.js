c716e3c1d43f96712dc5075d842d314e81140958
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-08-22 21:27:12 +0530

    DAY44

--- Projects/Weather APP/script.js
+++ Projects/Weather APP/script.js
@@ -14,11 +14,10 @@ const clouds_value = document.querySelector(".clouds-value");
 const search_button = document.querySelector('.search-button');
 const input_value = document.querySelector(".input");
 const option_container = document.querySelector(".option-container");
+const flag = document.querySelector(".country-image");
 let address;
 
 const API_key = "679c3ad87fd140a7b45163730252807";
-
-// http://api.weatherapi.com/v1/current.json?key=679c3ad87fd140a7b45163730252807&q=Pahasu&aqi=yes
 
 initial();
 
@@ -33,7 +32,7 @@ function initial(){
 
 }
 
-function getLocation() {
+function getLocation() {  
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
   } else {
@@ -42,18 +41,26 @@ function getLocation() {
 }
 
 async function display_information(result) {
-
   your_weather_container.style.display = "flex";
-  
-  
+
+  const countryName = result.location.country;
+
+  const code = countries[countryName];
+
+  if (code) {
+    flag.src = `https://flagcdn.com/48x36/${code.toLowerCase()}.png`;
+  } else {
+    flag.alt = "Flag not available";
+  }
+
   city_name.innerText = result.location.name;
   clouds.src = '//cdn.weatherapi.com/weather/64x64/night/116.png';
   temp.innerText = result.current.temp_c + "°C";
   windspeed.innerText = result.current.wind_kph + " kph";
   humidity.innerText = result.current.humidity + "%";
   clouds_value.innerText = result.current.cloud + "%";
-
 }
+
 
 
 async function call_api(lat, long){
@@ -64,9 +71,9 @@ async function call_api(lat, long){
     let data = await response.json();
 
 
-    address = data.display_name.split(" ")[0];
+    address = data.display_name;
 
-    let fetch_url = `http://api.weatherapi.com/v1/current.json?key=679c3ad87fd140a7b45163730252807&q=${address}&aqi=yes`;
+    let fetch_url = `https://api.weatherapi.com/v1/current.json?key=679c3ad87fd140a7b45163730252807&q=${address}&aqi=yes`;
 
     let result = await fetch(fetch_url);
     let weather_data = await result.json();
@@ -111,7 +118,7 @@ async function search_function() {
     
     let address = input_value.value;
 
-    let fetch_url = `http://api.weatherapi.com/v1/current.json?key=679c3ad87fd140a7b45163730252807&q=${address}&aqi=yes`;
+    let fetch_url = `https://api.weatherapi.com/v1/current.json?key=679c3ad87fd140a7b45163730252807&q=${address}&aqi=yes`;
 
     let data = await fetch(fetch_url);
 
@@ -129,7 +136,7 @@ async function your_weather_function() {
     search_weather_container.style.display = "none";
     loading_container.style.display = "flex";
 
-    let fetch_url = `http://api.weatherapi.com/v1/current.json?key=679c3ad87fd140a7b45163730252807&q=${address}&aqi=yes`;
+    let fetch_url = `https://api.weatherapi.com/v1/current.json?key=679c3ad87fd140a7b45163730252807&q=${address}&aqi=yes`;
 
     let data = await fetch(fetch_url);
 
@@ -141,3 +148,77 @@ async function your_weather_function() {
 }
 
 your_location_button.addEventListener('click', your_weather_function);
+
+
+const countries = {
+  "Afghanistan": "AF",
+  "Albania": "AL",
+  "Algeria": "DZ",
+  "Andorra": "AD",
+  "Angola": "AO",
+  "Argentina": "AR",
+  "Armenia": "AM",
+  "Australia": "AU",
+  "Austria": "AT",
+  "Azerbaijan": "AZ",
+  "Bangladesh": "BD",
+  "Belgium": "BE",
+  "Bhutan": "BT",
+  "Brazil": "BR",
+  "Bulgaria": "BG",
+  "Canada": "CA",
+  "Chile": "CL",
+  "China": "CN",
+  "Colombia": "CO",
+  "Croatia": "HR",
+  "Czech Republic": "CZ",
+  "Denmark": "DK",
+  "Egypt": "EG",
+  "Finland": "FI",
+  "France": "FR",
+  "Germany": "DE",
+  "Greece": "GR",
+  "Hong Kong": "HK",
+  "Hungary": "HU",
+  "Iceland": "IS",
+  "India": "IN",
+  "Indonesia": "ID",
+  "Iran": "IR",
+  "Iraq": "IQ",
+  "Ireland": "IE",
+  "Israel": "IL",
+  "Italy": "IT",
+  "Japan": "JP",
+  "Kazakhstan": "KZ",
+  "Kenya": "KE",
+  "Kuwait": "KW",
+  "Malaysia": "MY",
+  "Mexico": "MX",
+  "Nepal": "NP",
+  "Netherlands": "NL",
+  "New Zealand": "NZ",
+  "Nigeria": "NG",
+  "Norway": "NO",
+  "Pakistan": "PK",
+  "Philippines": "PH",
+  "Poland": "PL",
+  "Portugal": "PT",
+  "Qatar": "QA",
+  "Romania": "RO",
+  "Russia": "RU",
+  "Saudi Arabia": "SA",
+  "Singapore": "SG",
+  "South Africa": "ZA",
+  "South Korea": "KR",
+  "Spain": "ES",
+  "Sri Lanka": "LK",
+  "Sweden": "SE",
+  "Switzerland": "CH",
+  "Thailand": "TH",
+  "Turkey": "TR",
+  "Ukraine": "UA",
+  "United Arab Emirates": "AE",
+  "United Kingdom": "GB",
+  "United States": "US",
+  "Vietnam": "VN"
+};
